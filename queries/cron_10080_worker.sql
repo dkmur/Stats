6730de8e02990400df53fdfdf16be273041d0a2f
dkmur <42545952+dkmur@users.noreply.github.com>
2020-02-06 19:41:47 +0100

    Create cron_10080_worker.sql

---
+++ queries/cron_10080_worker.sql
@@ -0,0 +1,41 @@
+-- Weekly aggregation of worker data
+
+-- Settings For 10080min data 
+
+select @Datetime := concat(date(curdate() - interval weekday(curdate()) + 7 day),' ','00:00:00');
+
+INSERT INTO pogodb.stats_worker (Datetime,RPL, TRPL, Worker, Tmon, IVmon, Mon, Quest, Raid, Tloc,LocOk,LocNok,LocFR,Tp,TpOk,TpNok,TpFR,TpSt,Wk,WkOk,WkNok,WkFR,WkSt,Shiny,Res,Reb,ResTot,RebTot)
+select
+@Datetime,
+'10080',
+sum(TRPL),
+Worker,
+sum(Tmon),
+sum(IVmon),
+sum(Mon),
+sum(Quest),
+sum(Raid),
+sum(Tloc),
+sum(LocOk),
+sum(LocNok),
+sum(LocNok)/(sum(Tloc)+0.000001)*100,
+sum(Tp),
+sum(TpOk),
+sum(TpNok),
+sum(TpNok)/(sum(Tp)+0.000001)*100,
+sum(tpSt),
+sum(Wk),
+sum(WkOk),
+sum(WkNok),
+sum(WkNok)/(sum(Wk)+0.000001)*100,
+sum(WkSt),
+sum(Shiny),
+sum(Res),
+sum(Reb),
+max(ResTot),
+max(RebTot)
+
+from pogodb.stats_worker
+where Datetime >= date(curdate() - interval weekday(curdate()) + 7 day) and RPL = 1440
+group by Worker
+;
