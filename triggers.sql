6b7c05746406a5894abe7c0951254d7fa12d336a
dkmur <42545952+dkmur@users.noreply.github.com>
2020-02-11 15:30:39 +0100

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,7 +1,7 @@
 CREATE trigger pokemoncopy
 after insert
 on pokemon for each row
-insert into pogodb.pokemon_history_temp (encounter_id,spawnpoint_id,pokemon_id,latitude,longitude,disappear_time,individual_attack,individual_defense,individual_stamina,move_1,move_2,cp,cp_multiplier,weight,height,gender,form,costume,catch_prob_1,catch_prob_2,catch_prob_3,rating_attack,rating_defense,weather_boosted_condition,last_modified) 
+insert into pogodb.pokemon_history_temp (encounter_id,spawnpoint_id,pokemon_id,latitude,longitude,CONVERT_TZ(disappear_time, '+00:00', @@global.time_zone),individual_attack,individual_defense,individual_stamina,move_1,move_2,cp,cp_multiplier,weight,height,gender,form,costume,catch_prob_1,catch_prob_2,catch_prob_3,rating_attack,rating_defense,weather_boosted_condition,CONVERT_TZ(last_modified, '+00:00', @@global.time_zone)) 
 values (new.encounter_id,new.spawnpoint_id,new.pokemon_id,new.latitude,new.longitude,new.disappear_time,new.individual_attack,new.individual_defense,new.individual_stamina,new.move_1,new.move_2,new.cp,new.cp_multiplier,new.weight,new.height,new.gender,new.form,new.costume,new.catch_prob_1,new.catch_prob_2,new.catch_prob_3,new.rating_attack,new.rating_defense,new.weather_boosted_condition,new.last_modified)
 ;
 
@@ -10,7 +10,7 @@ after update
 on pokemon for each row
 update pogodb.pokemon_history_temp set
 pogodb.pokemon_history_temp.pokemon_id=new.pokemon_id,
-pogodb.pokemon_history_temp.disappear_time=new.disappear_time,
+CONVERT_TZ(pogodb.pokemon_history_temp.disappear_time, '+00:00', @@global.time_zone)=new.disappear_time,
 pogodb.pokemon_history_temp.individual_attack=new.individual_attack,
 pogodb.pokemon_history_temp.individual_defense=new.individual_defense,
 pogodb.pokemon_history_temp.individual_stamina=new.individual_stamina,
@@ -23,7 +23,7 @@ pogodb.pokemon_history_temp.gender=new.gender,
 pogodb.pokemon_history_temp.form=new.form,
 pogodb.pokemon_history_temp.costume=new.costume,
 pogodb.pokemon_history_temp.weather_boosted_condition=new.weather_boosted_condition,
-pogodb.pokemon_history_temp.last_modified=new.last_modified
+CONVERT_TZ(pogodb.pokemon_history_temp.last_modified, '+00:00', @@global.time_zone)=new.last_modified
 where
 pogodb.pokemon_history_temp.encounter_id=new.encounter_id
 ;
