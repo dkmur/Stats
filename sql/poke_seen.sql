fa2f2445697650ec03ebcf76acf6d4bf248a258d
dkmur <42545952+dkmur@users.noreply.github.com>
2020-04-12 14:20:03 +0200

    Update poke_seen.sql

--- sql/poke_seen.sql
+++ sql/poke_seen.sql
@@ -20,7 +20,12 @@ group by date(first_scanned)
 
 
 select
-Date as 'Date      ', Mon, Scanned, Shiny
+Date as 'Date      ',
+Mon,
+Scanned,
+Shiny,
+concat('1:',round(Scanned/Shiny,0)) as 'odds',
+round(100*Shiny/Scanned,4) as 'pct'
 from pogodb.tmp400
 ;
 select '';
@@ -29,7 +34,9 @@ select '';
 select
 Mon,
 sum(scanned) as 'scanned',
-sum(Shiny) as 'shiny'
+sum(Shiny) as 'shiny',
+concat('1:',round(sum(Scanned)/sum(Shiny),0)) as 'odds',
+round(100*sum(Shiny)/sum(Scanned),4) as 'pct'
 from pogodb.tmp400
 group by Mon
 ;
