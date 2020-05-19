83de5bd5789f6aa5224664ba4edecc735ec5ac0a
dkmur <dkmur74@gmail.com>
2020-05-20 00:00:00 +0200

    .

--- sql/atv_sw_overview.sql
+++ sql/atv_sw_overview.sql
@@ -11,7 +11,18 @@ date = curdate() - interval XXA day
 group by date, rom
 ;
 
-select "ATV RemoteGpsController" as '';
+select "ATV ROM options" as '';
+select
+date as 'date      ', rpad(pogo_update,11," ") as 'pogo_update', rpad(pd_update,9," ") as pd_update, rpad(rgc_update,10," ") as 'rgc_update', rpad(pingreboot,10," ") as pinreboot, count(origin) as 'devices'
+from ATVdetails
+
+where
+date = curdate() - interval XXA day
+
+group by date, pogo_update, pd_update, rgc_update, pingreboot
+;
+
+select "RemoteGpsController" as '';
 select
 date as 'date      ', rpad(rgc,8," ") as'rgc     ', count(origin) as 'devices'
 from ATVdetails
@@ -22,7 +33,7 @@ date = curdate() - interval XXA day
 group by date, rgc
 ;
 
-select "ATV PogoDroid" as '';
+select "PogoDroid" as '';
 select
 date as 'date      ', rpad(pogodroid,8," ") as 'pogodroid', count(origin) as 'devices'
 from ATVdetails
@@ -33,7 +44,7 @@ date = curdate() - interval XXA day
 group by date, pogodroid
 ;
 
-select "ATV PokemonGo" as '';
+select "PokemonGo" as '';
 select
 date as 'date      ', rpad(pogo,8," ") as 'pogo    ', count(origin) as 'devices'
 from ATVdetails
@@ -44,7 +55,7 @@ date = curdate() - interval XXA day
 group by date, pogo
 ;
 
-select "ATV Magisk" as '';
+select "Magisk" as '';
 select
 date as 'date      ', rpad(magisk,8," ") as 'magisk  ', count(origin) as 'devices'
 from ATVdetails
@@ -55,7 +66,7 @@ date = curdate() - interval XXA day
 group by date, magisk
 ;
 
-select "ATV Magisk Modules" as '';
+select "Magisk Modules" as '';
 select
 date as 'date      ', rpad(magisk_modules,34," ") as 'magisk modules                    ', count(origin) as 'devices'
 from ATVdetails
@@ -64,4 +75,15 @@ where
 date = curdate() - interval XXA day
 
 group by date, magisk_modules
+;
+
+select "Average temperature" as '';
+select
+date as 'date      ', round(sum(temperature)/count(temperature),1) as 'temperature'
+from ATVdetails
+
+where
+date = curdate() - interval XXA day
+
+group by date
 ;
