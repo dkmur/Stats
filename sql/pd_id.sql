6b26822da7007686bc215ffe61ec3c57cd2b4494
dkmur <42545952+dkmur@users.noreply.github.com>
2020-12-28 09:43:10 +0100

    Update pd_id.sql

--- sql/pd_id.sql
+++ sql/pd_id.sql
@@ -2,7 +2,7 @@ use pogodb
 
 select "Device PD ID" as '';
 select
-date as 'date      ', rpad(origin,13," ") as 'origin       ', ifnull(PD_auth_id,'no_data') as 'auth_id'
+datetime as 'datetime  ', rpad(origin,13," ") as 'origin       ', ifnull(PD_auth_id,'no_data') as 'auth_id'
 from ATVdetails
 
 where
