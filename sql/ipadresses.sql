2e08a46cc4327931905247eecbdab9773b40ffc3
dkmur <42545952+dkmur@users.noreply.github.com>
2020-12-28 09:50:24 +0100

    Update ipadresses.sql

--- sql/ipadresses.sql
+++ sql/ipadresses.sql
@@ -2,11 +2,11 @@ use pogodb
 
 select "Device IP" as '';
 select
-date as 'date      ', rpad(origin,13," ") as 'origin       ', ifnull(ip,'no_data') as 'IP'
+datetime as 'datetime  ', rpad(origin,13," ") as 'origin       ', ifnull(ip,'no_data') as 'IP'
 from ATVdetails
 
 where
-date = curdate() - interval XXA day
+date(datetime) = curdate() - interval XXA day
 
 order by origin
 ;
