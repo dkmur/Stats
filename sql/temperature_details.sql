829736e8fcec7a670d0954043ed53bcf00a92eba
dkmur <42545952+dkmur@users.noreply.github.com>
2021-01-01 18:14:08 +0100

    Update temperature_details.sql

--- sql/temperature_details.sql
+++ sql/temperature_details.sql
@@ -2,22 +2,22 @@ use pogodb
 
 select "Average temperature" as '';
 select
-date as 'date      ', round(sum(temperature)/count(temperature),1) as 'temperature'
+datetime as 'datetime  ', round(sum(temperature)/count(temperature),1) as 'temperature'
 from ATVdetails
 
 where
-date = curdate() - interval XXA day
+date(datetime) = curdate() - interval XXA day
 
-group by date
+group by datetime
 ;
 
 select "High 5" as '';
 select
-date as 'date      ', rpad(origin,13," ") as 'origin       ', temperature
+datetime as 'datetime  ', rpad(origin,13," ") as 'origin       ', temperature
 from ATVdetails
 
 where
-date = curdate() - interval XXA day
+date(datetime) = curdate() - interval XXA day
 
 order by temperature DESC
 limit 5
@@ -25,11 +25,11 @@ limit 5
 
 select "Low 5" as '';
 select
-date as 'date      ', rpad(origin,13," ") as 'origin      ', temperature
+datetime as 'datetime  ', rpad(origin,13," ") as 'origin      ', temperature
 from ATVdetails
 
 where
-date = curdate() - interval XXA day and
+date(datime) = curdate() - interval XXA day and
 temperature is not NULL and
 temperature <> ''
 
@@ -39,11 +39,11 @@ limit 5
 
 select "By temperature" as '';
 select
-date as 'date      ', rpad(ifnull(temperature,'no_data'),11," ") as 'temperature', count(origin) as '#devices' 
+datetime as 'datetime  ', rpad(ifnull(temperature,'no_data'),11," ") as 'temperature', count(origin) as '#devices' 
 from ATVdetails
 
 where
-date = curdate() - interval XXA day
+date(datetime) = curdate() - interval XXA day
 
-group by date, temperature
+group by datetime, temperature
 ;
