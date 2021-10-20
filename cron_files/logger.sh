412cd8c3b49a725409e5b34c6e95a47d52f2ba59
dkmur <dkmur@v2202008102643124981.goodsrv.de>
2021-10-20 16:29:36 +0200

    daily header

---
+++ cron_files/logger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+folder="$(cd ../ && pwd)"
+source $folder/config.ini
+
+# Logging
+mkdir -p $PATH_TO_STATS/logs
+touch $PATH_TO_STATS/logs/log_$(date '+%Y%m').log
+echo " " >> $PATH_TO_STATS/logs/log_$(date '+\%Y\%m').log
+echo "#########################          $(date '+%Y-%m-%d')           #########################" >> $PATH_TO_STATS/logs/log_$(date '+\%Y\%m').log
+echo "Start time          Stop time           Duration  Process" >> $PATH_TO_STATS/logs/log_$(date '+\%Y\%m').log
+echo " " >> $PATH_TO_STATS/logs/log_$(date '+\%Y\%m').log
