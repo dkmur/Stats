cc1dc97023f615a25e17328c215ba4566466d61e
dkmur <dkmur74@gmail.com>
2022-03-26 10:21:03 +0100

    allow weekly backup pokemon_history

--- cron_files/44640.sh
+++ cron_files/44640.sh
@@ -8,7 +8,8 @@ mkdir -p $PATH_TO_STATS/logs
 touch $PATH_TO_STATS/logs/log_$(date '+%Y%m').log
 
 # Monthly backup pokemon_history
-if "$monthly_mon_table"
+
+if $monthly_mon_table && ! $weekly_backup
 then
   start=$(date '+%Y%m%d %H:%M:%S')
   yearmonth=$(date --date="yesterday" +"%Y_%m")
@@ -17,8 +18,8 @@ then
     mysqldump -h$DB_IP -P$DB_PORT -u$SQL_user $STATS_DB --no-data pokemon_history > $PATH_TO_STATS\pokemon_history_structure.sql
     mysql -h$DB_IP -P$DB_PORT -u$SQL_user $STATS_DB -e "alter table $STATS_DB.pokemon_history RENAME $monthly_mon_database.pokemon_history_$yearmonth"
     mysql -h$DB_IP -P$DB_PORT -u$SQL_user $STATS_DB < $PATH_TO_STATS\pokemon_history_structure.sql
-    mysqloptimize --skip-write-binlog -h$DB_IP -P$DB_PORT -u$SQL_user $monthly_mon_database pokemon_history_$yearmonth
     rm $PATH_TO_STATS/pokemon_history_structure.sql
+#    mysqloptimize --skip-write-binlog -h$DB_IP -P$DB_PORT -u$SQL_user $monthly_mon_database pokemon_history_$yearmonth
     stop=$(date '+%Y%m%d %H:%M:%S')
     diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
     echo "[$start] [$stop] [$diff] Monthly backup pokemon_history" >> $PATH_TO_STATS/logs/log_$(date '+%Y%m').log
@@ -30,13 +31,15 @@ then
       rm $monthly_mon_folder/pokemon_history_$yearmonth.sql
       mysql -h$DB_IP -P$DB_PORT -u$SQL_user $monthly_mon_database -e "drop table pokemon_history_$yearmonth"
       echo "[$start] [$stop] [$diff] Monthly backup pokemon_history dumped, zipped and removed" >> $PATH_TO_STATS/logs/log_$(date '+%Y%m').log
+    else
+      mysqloptimize --skip-write-binlog -h$DB_IP -P$DB_PORT -u$SQL_user $monthly_mon_database pokemon_history_$yearmonth
     fi
   else
     mysqldump -h$DB_IP -P$DB_PORT -u$SQL_user -p$SQL_password $STATS_DB --no-data pokemon_history > $PATH_TO_STATS\pokemon_history_structure.sql
     mysql -h$DB_IP -P$DB_PORT -u$SQL_user -p$SQL_password $STATS_DB -e "alter table $STATS_DB.pokemon_history RENAME $monthly_mon_database.pokemon_history_$yearmonth"
     mysql -h$DB_IP -P$DB_PORT -u$SQL_user -p$SQL_password $STATS_DB < $PATH_TO_STATS\pokemon_history_structure.sql
     rm $PATH_TO_STATS/pokemon_history_structure.sql
-    mysqloptimize --skip-write-binlog -h$DB_IP -P$DB_PORT -u$SQL_user -p$SQL_password $monthly_mon_database pokemon_history_$yearmonth
+#    mysqloptimize --skip-write-binlog -h$DB_IP -P$DB_PORT -u$SQL_user -p$SQL_password $monthly_mon_database pokemon_history_$yearmonth
     stop=$(date '+%Y%m%d %H:%M:%S')
     diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
     echo "[$start] [$stop] [$diff] Monthly backup pokemon_history" >> $PATH_TO_STATS/logs/log_$(date '+%Y%m').log
@@ -48,6 +51,8 @@ then
       rm $monthly_mon_folder/pokemon_history_$yearmonth.sql
       mysql -h$DB_IP -P$DB_PORT -u$SQL_user -p$SQL_password $monthly_mon_database -e "drop table pokemon_history_$yearmonth"
       echo "[$start] [$stop] [$diff] Monthly backup pokemon_history dumped, zipped and removed" >> $PATH_TO_STATS/logs/log_$(date '+%Y%m').log
+    else
+      mysqloptimize --skip-write-binlog -h$DB_IP -P$DB_PORT -u$SQL_user -p$SQL_password $monthly_mon_database pokemon_history_$yearmonth
     fi
   fi
 fi
