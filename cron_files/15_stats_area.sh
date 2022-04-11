1eebd2ee4c8581a1a145ae08f747810673c4d2f3
dkmur <dkmur74@gmail.com>
2022-04-12 00:12:56 +0200

    processing twice doesn't make stats look any better

--- cron_files/15_stats_area.sh
+++ cron_files/15_stats_area.sh
@@ -35,12 +35,3 @@ stop=$(date '+%Y%m%d %H:%M:%S')
 diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
 echo "[$start] [$stop] [$diff] Stats rpl15 quest area processing" >> $PATH_TO_STATS/logs/log_$(date '+%Y%m').log
 fi
-
-# rpl 15 worker stats
-start=$(date '+%Y%m%d %H:%M:%S')
-cat $PATH_TO_STATS/cron_files/15_worker.sql | query
-stop=$(date '+%Y%m%d %H:%M:%S')
-diff=$(printf '%02dm:%02ds\n' $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))/60)) $(($(($(date -d "$stop" +%s) - $(date -d "$start" +%s)))%60)))
-echo "[$start] [$stop] [$diff] Stats rpl15 worker processing" >> $PATH_TO_STATS/logs/log_$(date '+%Y%m').log
-
-
